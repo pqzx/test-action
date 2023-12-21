cf5418b3372a2107fbb95fe22a2dccd4d0aa00a4
Jin Cong <jin@sitehive.co>
2023-12-22 08:14:34 +1100

    Add another step to the action

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,3 +4,21 @@ echo "Hello $1"
 time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
 
+# Get the script path from the 2nd argument
+script_path=$2
+
+# Check if the provided path exists
+if [ ! -f "$script_path" ]; then
+    echo "Error: Script not found at '$script_path'"
+    exit 1
+fi
+
+# Check if the script is executable
+if [ ! -x "$script_path" ]; then
+    echo "Error: Script at '$script_path' is not executable"
+    exit 1
+fi
+
+# Execute the script
+echo "Executing script: $script_path"
+"$script_path"
